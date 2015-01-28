8bd7e5444c3af37805bfcb7ba7760c71582a9ba5
Tom Hicks <hickst@tohono.com>
2015-01-28 16:13:33 -0700

    Use local annotate method with no co-reference resolution.

--- src/main/scala/edu/arizona/sista/processors/DiscourseParserRunner.scala
+++ src/main/scala/edu/arizona/sista/processors/DiscourseParserRunner.scala
@@ -7,7 +7,7 @@ import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
 /**
  * External API for running different discourse parsers for visualization.
  * Written By: Tom Hicks. 1/15/2015.
- * Last Modified: Rename discourse parser runner class, add syntax trees to results.
+ * Last Modified: Use local annotate method with no co-reference resolution.
  */
 class DiscourseParserRunner (useProcessor:String = "core") {
   val processor:Processor =
@@ -17,25 +17,38 @@ class DiscourseParserRunner (useProcessor:String = "core") {
       new CoreNLPProcessor(withDiscourse = true)
 
 
-  def parseText(text: String): DiscourseParserResults = {
-    // annotate the document using the selected processor
-    val doc = processor.annotate(text)
+  def parseText (text: String): DiscourseParserResults = {
+    // create and annotate a document using the selected processor
+    val doc = annotateNoCoRef(processor.mkDocument(text))
     // return information from discourse trees as an array of JSON strings:
     new DiscourseParserResults(text, discTrees(doc), synTrees(doc))
   }
 
-  def discTrees(doc: Document): Array[String] = {
+  def discTrees (doc: Document): Array[String] = {
     val allTrees = doc.discourseTree map { dTree =>
       dTree.visualizerJSON()
     }
     allTrees.toArray
   }
 
-  def synTrees(doc: Document): Array[String] = {
+  def synTrees (doc: Document): Array[String] = {
     val allTrees = doc.sentences map { s =>
       s.syntacticTree.getOrElse("()").toString()
     }
     allTrees.toArray
+  }
+
+
+  def annotateNoCoRef (doc:Document): Document = {
+    processor.tagPartsOfSpeech(doc)
+    processor.lemmatize(doc)
+    processor.recognizeNamedEntities(doc)
+    processor.parse(doc)
+    processor.chunking(doc)
+    processor.labelSemanticRoles(doc)
+    processor.discourse(doc)
+    doc.clear()
+    doc
   }
 
   override def toString:String = {
