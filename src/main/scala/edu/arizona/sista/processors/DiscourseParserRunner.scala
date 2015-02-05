994e2de637b26df8898af1018e4c249525c84592
Tom Hicks <hickst@tohono.com>
2015-02-05 13:46:53 -0700

    Create own annotate method to log time for all phases.

--- src/main/scala/edu/arizona/sista/processors/DiscourseParserRunner.scala
+++ src/main/scala/edu/arizona/sista/processors/DiscourseParserRunner.scala
@@ -7,7 +7,7 @@ import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
 /**
  * External API for running different discourse parsers for visualization.
  * Written By: Tom Hicks. 1/15/2015.
- * Last Modified: Revert to using co-reference, add discourse tree print logging.
+ * Last Modified: Create own annotate method to log time for all phases.
  */
 class DiscourseParserRunner (useProcessor:String = "core") {
   val processor:Processor =
@@ -19,16 +19,20 @@ class DiscourseParserRunner (useProcessor:String = "core") {
 
   def parseText (text: String): DiscourseParserResults = {
     // create and annotate a document using the selected processor
-    val doc = processor.annotate(text);
-    // val doc = annotateNoCoRef(processor.mkDocument(text))  // call to bypass co-ref processing
+    val start = System.currentTimeMillis()
+    val doc = myAnnotate(processor.mkDocument(text))  // call for custom processing
+    // val doc = processor.annotate(text)
+    val stop = System.currentTimeMillis()
+    val elapsed = stop - start
+    println(this.toString()+": [ANNOT]: %5d, %d, %d".format(elapsed, start, stop))
 
     doc.discourseTree.foreach(dt => {
-      println("Document-wide discourse tree: returned from processor.annotate (with coRef)")
+      println(this.toString()+": Discourse tree from processor.annotate (with coRef)")
       println(dt.toString())
     })
 
     // return information from discourse trees as an array of JSON strings:
-    new DiscourseParserResults(text, discTrees(doc), synTrees(doc))
+    new DiscourseParserResults(text, elapsed, discTrees(doc), synTrees(doc))
   }
 
   def discTrees (doc: Document): Array[String] = {
@@ -47,22 +51,54 @@ class DiscourseParserRunner (useProcessor:String = "core") {
 
 
   // this alternate annotate method can be called from above to bypass co-reference processing
-  def annotateNoCoRef (doc:Document): Document = {
-    processor.tagPartsOfSpeech(doc)
-    processor.lemmatize(doc)
-    processor.recognizeNamedEntities(doc)
-    processor.parse(doc)
-    processor.chunking(doc)
-    processor.labelSemanticRoles(doc)
-    processor.discourse(doc)
+  def myAnnotate (doc:Document): Document = {
+    var t = Map[String, java.lang.Long]()
+
+    t = timeIt { processor.tagPartsOfSpeech(doc) }
+    println(this.toString()+":   [POS]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.lemmatize(doc) }
+    println(this.toString()+": [Lemma]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.recognizeNamedEntities(doc) }
+    println(this.toString()+":   [NER]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.parse(doc) }
+    println(this.toString()+": [Parse]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.chunking(doc) }
+    println(this.toString()+": [Chunk]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.labelSemanticRoles(doc) }
+    println(this.toString()+": [Roles]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.resolveCoreference(doc) }
+    println(this.toString()+": [CoRef]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
+    t = timeIt { processor.discourse(doc) }
+    println(this.toString()+": [DiscP]: %5d, %d, %d".format(t("elapsed"), t("start"), t("stop")))
+
     doc.clear()
     doc
   }
 
+
+  // return a tuple of start time, stop time, and elapsed time for the given block
+  def timeIt (it: => Unit): Map[String, java.lang.Long] = {
+    val start = System.currentTimeMillis()
+    it
+    val stop = System.currentTimeMillis()
+    return Map("start"->start, "stop"->stop, "elapsed"->(stop-start))
+  }
+
+
   override def toString:String = {
-    return "<%s: %s>".format(super.toString(), useProcessor)
+    return "<%s:%s>".format(super.getClass().getSimpleName(), processor.getClass.getSimpleName())
   }
 }
 
 
-class DiscourseParserResults(val text:String, val dTrees:Array[String], val synTrees:Array[String])
+class DiscourseParserResults(val text:String,
+                             val annoTime:java.lang.Long,
+                             val dTrees:Array[String],
+                             val synTrees:Array[String])
