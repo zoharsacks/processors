8247d37354329c3b0c2551a8afa6169a04c8ad74
Tom Hicks <hickst@tohono.com>
2015-02-07 17:33:55 -0700

    Simplify timings to list of lists, return JSON. Remove package object.

--- src/main/scala/edu/arizona/sista/processors/visualizer/DiscourseParserRunner.scala
+++ src/main/scala/edu/arizona/sista/processors/visualizer/DiscourseParserRunner.scala
@@ -2,16 +2,19 @@ package edu.arizona.sista.processors.visualizer
 
 import scala.collection.JavaConverters._
 
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+
 import edu.arizona.sista.processors._
 import edu.arizona.sista.processors.bionlp.BioNLPProcessor
 import edu.arizona.sista.processors.corenlp.CoreNLPProcessor
 import edu.arizona.sista.processors.fastnlp.FastNLPProcessor
-import edu.arizona.sista.processors.visualizer._  // import package object
 
 /**
  * External API for running different discourse parsers for visualization.
  * Written By: Tom Hicks. 1/15/2015.
- * Last Modified: Return map of timings in moved discourse parser results.
+ * Last Modified: Simplify timings to list of lists, return JSON.
  */
 class DiscourseParserRunner (useProcessor:String = "core") {
   val processor:Processor =
@@ -25,11 +28,11 @@ class DiscourseParserRunner (useProcessor:String = "core") {
     // create and annotate a document using the selected processor
     val start = System.currentTimeMillis()
     // val doc = processor.annotate(text)
-    // var timings:TimingsType = TimingsType()
+    // var timings:List[List[Any]] = List()
     var (doc, timings) = myAnnotate(processor.mkDocument(text)) // call for custom processing
     val stop = System.currentTimeMillis()
     val elapsed = stop - start
-    timings ++= List("ANNOT.start"-> start, "ANNOT.stop"->stop, "ANNOT.elapsed"->elapsed)
+    timings = List("ANNOT", elapsed, start, stop) +: timings
 //    println(this.toString()+": [ANNOT]: %5d, %d, %d".format(elapsed, start, stop))
 
     doc.discourseTree.foreach(dt => {
@@ -38,7 +41,24 @@ class DiscourseParserRunner (useProcessor:String = "core") {
     })
 
     // return information from discourse trees as an array of JSON strings:
-    new DiscourseParserResults(text, timings.asJava, discTrees(doc), synTrees(doc))
+    new DiscourseParserResults(text, toJson(timings), discTrees(doc), synTrees(doc))
+  }
+
+
+  // this alternate annotate method can be called from above to customize processing
+  def myAnnotate (doc:Document): (Document, List[List[Any]]) = {
+    var t:List[List[Any]] = List(
+      "POS"   +: timeIt { processor.tagPartsOfSpeech(doc) },
+      "Lemma" +: timeIt { processor.lemmatize(doc) },
+      "NER"   +: timeIt { processor.recognizeNamedEntities(doc) },
+      "Parse" +: timeIt { processor.parse(doc) },
+      "Chunk" +: timeIt { processor.chunking(doc) },
+      "Roles" +: timeIt { processor.labelSemanticRoles(doc) },
+      "CoRef" +: timeIt { processor.resolveCoreference(doc) },
+      "DiscP" +: timeIt { processor.discourse(doc) }
+    )
+    doc.clear()
+    return (doc, t)
   }
 
   def discTrees (doc: Document): Array[String] = {
@@ -55,43 +75,39 @@ class DiscourseParserRunner (useProcessor:String = "core") {
     allTrees.toArray
   }
 
-
-  // this alternate annotate method can be called from above to customize processing
-  def myAnnotate (doc:Document): (Document, TimingsType) = {
-    var t:TimingsType = TimingsType()
-
-    t ++= timeIt("POS",   { processor.tagPartsOfSpeech(doc) })
-    t ++= timeIt("Lemma", { processor.lemmatize(doc) })
-    t ++= timeIt("NER",   { processor.recognizeNamedEntities(doc) })
-    t ++= timeIt("Parse", { processor.parse(doc) })
-    t ++= timeIt("Chunk", { processor.chunking(doc) })
-    t ++= timeIt("Roles", { processor.labelSemanticRoles(doc) })
-    t ++= timeIt("CoRef", { processor.resolveCoreference(doc) })
-    t ++= timeIt("DiscP", { processor.discourse(doc) })
-
-    doc.clear()
-    return (doc, t)
-  }
-
-
   // return a map of start time, stop time, and elapsed time for the given block
-  def timeIt (prefix:String, it: => Unit): TimingsType = {
+  def timeIt (it: => Unit): List[Long] = {
     val start = System.currentTimeMillis()
     it
     val stop = System.currentTimeMillis()
-    return scala.collection.mutable.Map((s"${prefix}.start")->start,
-                                        (s"${prefix}.stop")->stop,
-                                        (s"${prefix}.elapsed")->(stop-start))
+    return List((stop-start), start, stop)
   }
 
+  // return a JSON string from the given list of lists
+  def toJson (timings:List[List[Any]], pprint:Boolean=false): String = {
+    val jTimings:List[JValue] = timings.map(it => tListToJson(it))
+    if (pprint)
+      pretty(render(jTimings))
+    else
+      compact(render(jTimings))
+  }
 
+  def tListToJson (lst:List[Any]): JArray = {
+    JArray(List(JString(lst(0).toString),
+                JInt(BigInt(lst(1).asInstanceOf[Long])),
+                JInt(BigInt(lst(2).asInstanceOf[Long])),
+                JInt(BigInt(lst(3).asInstanceOf[Long]))))
+  }
+
+  // override and customize string representation of this class
   override def toString:String = {
     return "<%s:%s>".format(super.getClass().getSimpleName(), processor.getClass.getSimpleName())
   }
 }
 
 
+/** Class to hold parser results. */
 class DiscourseParserResults(val text:String,
-                             val timings:java.util.Map[String, java.lang.Long],
+                             val timings:String,
                              val dTrees:Array[String],
                              val synTrees:Array[String])
